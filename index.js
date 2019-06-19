35d3988da5369c26affd6c9298ef5cf9a5970e40
swislockikacper <swislocki.kacper@gmail.com>
2019-06-19 12:42:32 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const expressEdge = require("express-edge");
+const express = require("express");
+
+const app = new express();
+
+app.use(express.static("public"));
+app.use(expressEdge);
+app.set("views", `${__dirname}/views`);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+});
+
+app.get("/post", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+});
+
+app.listen(4000, () => console.log(`App working on port ${4000}`));
