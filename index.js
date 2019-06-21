a0683ceda9e20cfc8d32e2ee9644be3af56a601b
swislockikacper <swislocki.kacper@gmail.com>
2019-06-21 08:04:54 +0200

    added mongo

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const path = require("path");
 const expressEdge = require("express-edge");
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const Post = require("./database/models/Post");
+const fileUpload = require("express-fileupload");
 
 const app = new express();
 
+mongoose
+  .connect("mongodb://localhost:27017/node-blog", { useNewUrlParser: true })
+  .then(() => console.log("You are now connected to Mongo!"))
+  .catch(err => console.error("Something went wrong", err));
+
+app.use(fileUpload());
 app.use(express.static("public"));
 app.use(expressEdge);
 app.set("views", `${__dirname}/views`);
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/new", (req, res) => {
+  res.render("create");
 });
 
 app.get("/about", (req, res) => {
@@ -22,6 +38,37 @@ app.get("/contact", (req, res) => {
 
 app.get("/post", (req, res) => {
   res.sendFile(path.resolve(__dirname, "pages/post.html"));
+});
+
+app.post("/store", (req, res) => {
+  const { image } = req.files;
+
+  image.mv(path.resolve(__dirname, "public/posts", image.name), error => {
+    Post.create(
+      {
+        ...req.body,
+        image: `/posts/${image.name}`
+      },
+      (error, post) => {
+        res.redirect("/");
+      }
+    );
+  });
+});
+
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+
+  res.render("index", {
+    posts
+  });
+});
+
+app.get("/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("post", {
+    post
+  });
 });
 
 app.listen(4000, () => console.log(`App working on port ${4000}`));
